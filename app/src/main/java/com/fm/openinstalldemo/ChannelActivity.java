5e9bf3277fcc0a6c060e18a1dfe10b9effbadfe8
wade <wade@openinstall.io>
2023-01-04 21:16:17 +0800

    2.7.0

--- app/src/main/java/com/fm/openinstalldemo/ChannelActivity.java
+++ app/src/main/java/com/fm/openinstalldemo/ChannelActivity.java
@@ -1,6 +1,7 @@
 package com.fm.openinstalldemo;
 
 import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -9,10 +10,14 @@ import android.view.View;
 
 import com.fm.openinstall.OpenInstall;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ChannelActivity extends AppCompatActivity {
 
     Toolbar toolbar;
-    public static final String POINT_ID = "effect_test";
+    public static final String POINT_TEST = "effect_test";
+    public static final String POINT_DETAIL = "effect_detail";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -33,7 +38,7 @@ public class ChannelActivity extends AppCompatActivity {
         findViewById(R.id.register).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //注册上报
+                // 注册上报
                 OpenInstall.reportRegister();
                 showReportDialog();
             }
@@ -42,8 +47,21 @@ public class ChannelActivity extends AppCompatActivity {
         findViewById(R.id.effect).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //效果点统计
-                OpenInstall.reportEffectPoint(POINT_ID, 1);
+                // 效果点统计
+                OpenInstall.reportEffectPoint(POINT_TEST, 1);
+                showReportDialog();
+            }
+        });
+
+        findViewById(R.id.detail).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 效果点自定义参数：需要控制台的效果点管理中启用“记录明细”并且添加“自定义参数”
+                Map<String, String> extraMap = new HashMap<>();
+                extraMap.put("brand", Build.BRAND);
+                extraMap.put("model", Build.MODEL);
+                // 效果点统计
+                OpenInstall.reportEffectPoint(POINT_DETAIL, 1, extraMap);
                 showReportDialog();
             }
         });
